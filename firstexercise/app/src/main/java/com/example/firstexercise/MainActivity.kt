0b315e813fd9e53fcf96f87581e67ceffcbe5699
Lê Bá Phước Long <long.le@logixtek.com>
2022-03-10 22:26:47 +0700

    mainLayout

--- firstexercise/app/src/main/java/com/example/firstexercise/MainActivity.kt
+++ firstexercise/app/src/main/java/com/example/firstexercise/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.example.firstexercise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.Window
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firstexercise.features.welcome.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var i = 0
+    private var handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportActionBar?.hide();
         setContentView(R.layout.activity_main)
-//        this activity will display "SplashLayout" delay 2s then redirect to
-//        welcomeLayout
+        progressBar = findViewById<ProgressBar>(R.id.loadingIntoApp) as ProgressBar
+        progressBar!!.visibility =View.VISIBLE;
+        progressBar!!.max = 100;
+        i = progressBar!!.progress;
+
+        Thread(Runnable {
+
+            while (i < 100) {
+                i += 1
+                handler.post(Runnable {
+                    progressBar!!.progress=i;
+                })
+                try {
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+
+            progressBar!!.visibility = View.INVISIBLE
+
+            val intent = Intent(this, WelcomeActivity::class.java);
+            startActivity(intent)
+        }).start()
+
     }
 }
