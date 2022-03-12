7ae666a2da7e5214aac4a224475fe998d7eff537
Lê Bá Phước Long <long.le@logixtek.com>
2022-03-12 18:25:28 +0700

    finally

--- firstexercise/app/src/main/java/com/example/firstexercise/features/onboardings/OnboardOneActivity.kt
+++ firstexercise/app/src/main/java/com/example/firstexercise/features/onboardings/OnboardOneActivity.kt
@@ -2,11 +2,14 @@ package com.example.firstexercise.features.onboardings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import com.example.firstexercise.R
 
 class OnboardOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_onboard_one)
     }
 }
